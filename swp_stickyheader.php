e6a7e2bb57958eb1802a93f2abf127830b6d6179
Mark Corpuz <social@markcorpuz.com>
2018-02-11 08:23:01 -0800

    Initial Commit
    Renamed this plugin to swp-stickyheader. We might have multiple variations of this plugin so it makes sense to go ahead and start keeping track of these updates.

---
+++ swp_stickyheader.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name: SWP - Sticky Header
+ * Description: Change header's appearance upon scrolling
+ * Version: 1.0
+ * Author: Jake Almeda
+ * Author URI: http://smarterwebpackages.com/
+ * Network: true
+ * License: GPL2
+ */
+
+
+class InfinityHeaderCopy {
+
+	public $addtheseclasses    = 'white',
+			$addthemwhentopisat = '50';
+
+	public function swp_infini_hack_scripts() {
+		
+	    //wp_enqueue_script( 'swp_infiniheader', plugins_url( 'js/infinity_hack.js', __FILE__ ), NULL, '1.0', TRUE );
+	   	wp_register_script( 'swp_infiniheader', plugins_url( 'js/infinity_hack.js', __FILE__ ), NULL, '1.0', TRUE );
+	     
+	    // Localize the script with new data
+	    $extra_var = array(
+	        'swp_addtheseclasses' 	=> $this->addtheseclasses,
+	        'swp_whentopison'	 	=> $this->addthemwhentopisat,
+	    );
+	    wp_localize_script( 'swp_infiniheader', 'infini_extra_var', $extra_var );
+	     
+	    // Enqueued script with localized data.
+	    wp_enqueue_script( 'swp_infiniheader' );
+
+	}
+
+	public function __construct() {
+		
+        // enqueue scripts
+		add_action( "wp_enqueue_scripts", array( $this, "swp_infini_hack_scripts" ) );
+
+    }
+
+}
+
+$infinity_header = new InfinityHeaderCopy();
